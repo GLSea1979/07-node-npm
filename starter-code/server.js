77ee6be268b2f84c6f821a2ebccd40b88a60cce5
Gary Lundgren <builtbygl@gmail.com>
2017-01-10 15:38:53 -0800

    finished todos

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,20 +1,30 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
-
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// done: Initialize your project using NPM to create and populate a package.json file
+// done: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
+const express = require('express');
+const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+// done: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
 
-app.get('*', function(request, response) {
-  // TODO: Using the response object, send the index.html file back to the user
+app.get('/index.html', function(request, response) {
+  // done: Using the response object, send the index.html file back to the user
+  response.sendFile('index.html', {root: './public'});
 });
 
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
+// done: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('new.html', function(request, response) {
+  response.sendFile('new.html', {root: './public'});
+});
+app.get('*', function(request, response){
+  console.log('New request', request.url);
+  response.sendFile('404.html', {root: './public'});
+})
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  // done: Log to the console a message that lets you know which port your server has started on
+  console.log('server is up and running. and can be accessed at localhost:5000');
 });
